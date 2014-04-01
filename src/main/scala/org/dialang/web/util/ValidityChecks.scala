543c1ae09a3a50a5a34f8746a6ec7f112f7159c5
Adrian Fish <adrian.r.fish@gmail.com>
2014-04-01 11:36:58 +0100

    Added launch checks for valid al, tl and skill.

---
+++ src/main/scala/org/dialang/web/util/ValidityChecks.scala
@@ -0,0 +1,12 @@
+package org.dialang.web.util
+
+import org.dialang.web.db.DBFactory
+
+object ValidityChecks {
+
+  val db = DBFactory.get()
+
+  def adminLanguageExists(al: String): Boolean = db.adminLanguages.contains(al)
+  def testLanguageExists(tl: String): Boolean = db.testLanguages.contains(tl)
+  def skillExists(skill: String): Boolean = db.skills.contains(skill)
+}
