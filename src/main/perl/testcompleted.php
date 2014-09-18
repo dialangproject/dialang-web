34e6dbae36069b45f1063619c45a7009009e381a
Adrian Fish <adrian.r.fish@gmail.com>
2014-09-18 14:32:21 +0100

    Added the tes and completed example scripts.

---
+++ src/main/perl/testcompleted.php
@@ -0,0 +1,12 @@
+<?php
+$id = $_POST['id'];
+$theirhash = $_POST['hash'];
+
+$ourhash = base64_encode(hash_hmac('sha1', $id . 'adrian_moodle', 'secret', true));
+
+if ($ourhash == $theirhash) {
+	error_log("OK !!!!!!!!");
+} else {
+	error_log("NOT OK !!!!!!!!");
+}
+?>
