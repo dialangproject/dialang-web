9d504e107d0af1ce536f381c083c4f5194548ba2
Adrian Fish <adrian.r.fish@gmail.com>
2015-09-22 14:15:59 +0100

    ELSB-214 Fixed the instant feedback button

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,6 @@
 
 cd ..
 gradle --offline -x test build
-sudo -S cp build/libs/dialang-web.war /srv/dialang-tomcat/webapps/ROOT.war
+sudo -S cp build/libs/dialang-web.war /srv/dialang-tomcat1/webapps/ROOT.war
+sudo -S cp build/libs/dialang-web.war /srv/dialang-tomcat2/webapps/ROOT.war
 
