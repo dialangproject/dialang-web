5f5d1de968558a776cfcf14a54da703411625b02
Adrian Fish <adrian.r.fish@gmail.com>
2017-09-06 15:23:15 +0100

    Removed blank line

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,3 @@ cd ..
 gradle --offline -x test build
 sudo -S cp build/libs/dialang-web.war /srv/dialang-tomcat1/webapps/ROOT.war
 sudo -S cp build/libs/dialang-web.war /srv/dialang-tomcat2/webapps/ROOT.war
-
